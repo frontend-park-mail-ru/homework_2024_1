b0fb82d0fb427c6108acfda373426146c5ae7bc0
Alladan04 <alla.danielyan.2004@mail.ru>
2024-02-11 02:48:46 +0300

    fixed

--- source/plain.js
+++ source/plain.js
@@ -1,3 +1,18 @@
 'use strict';
-
-const plain = array=> array.flat(Infinity)
+const plain_inner = (result, array)=>{
+	array.forEach(item=>{
+		if (Array.isArray(item))
+			plain_inner(result,item);
+		
+		else 
+			result.push(item);
+			
+	   		
+	}
+	)
+}
+const plain =  (array)=> {
+	let result = []
+	plain_inner(result, array)
+	return result;
+ } 
