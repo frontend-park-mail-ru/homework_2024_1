190ce6454e44e09573e907fad0c2acb985ecb05d
mizhgun <m.a.ugryumov@outlook.com>
2024-02-10 18:13:11 +0300

    type check fixed & code style

--- source/tree.js
+++ source/tree.js
@@ -7,19 +7,20 @@ const TRUNK_SYMBOL = '|';
 
 const buildLayer = (width, layerWidth, layerSymbol) => {
     const emptySpace = SPACE_SYMBOL.repeat(Math.floor((width - layerWidth) / 2));
-    const nonEmptySpace = layerSymbol.repeat(layerWidth);
+    const layerSpace = layerSymbol.repeat(layerWidth);
 
-    return emptySpace + nonEmptySpace + emptySpace + ENDLINE_SYMBOL;
+    return emptySpace + layerSpace + emptySpace + ENDLINE_SYMBOL;
 }
 
 const tree = (size) => {
-    if (!["number", "string"].includes(typeof size) || !parseInt(size) || size < 3) {
+    if (!["number", "string"].includes(typeof size) || isNaN(parseInt(size, 10)) || size < 3) {
         return null;
     }
 
     const width = 2 * size - 3;
 
     let result = '';
+
     for (let layer = 1; layer < size; ++layer) {
         const treeLayerWidth = 2 * layer - 1;
         result += buildLayer(width, treeLayerWidth, TREE_SYMBOL);
