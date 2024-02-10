089f17d88d46d8eedcab98ccacc87787a12b5739
mizhgun <m.a.ugryumov@outlook.com>
2024-02-10 17:06:05 +0300

    consts moved

--- source/tree.js
+++ source/tree.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const buildLayer = (width, layerWidth, layerSymbol, spaceSymbol, endlineSymbol) => {
+const buildLayer = (width, layerWidth, layerSymbol) => {
+    const endlineSymbol = '\n';
+    const spaceSymbol = ' ';
+
     const emptySpace = spaceSymbol.repeat(Math.floor((width - layerWidth) / 2));
+
     return emptySpace + layerSymbol.repeat(layerWidth) + emptySpace + endlineSymbol;
 }
 
@@ -12,19 +16,17 @@ const tree = (size) => {
 
     const width = 2 * size - 3;
 
-    const endlineSymbol = '\n';
-    const spaceSymbol = ' ';
     const treeSymbol = '*';
     const trunkSymbol = '|';
 
     let result = '';
     for (let layer = 0; layer < size - 1; ++layer) {
         const treeLayerWidth = 2 * layer + 1;
-        result += buildLayer(width, treeLayerWidth, treeSymbol, spaceSymbol, endlineSymbol);
+        result += buildLayer(width, treeLayerWidth, treeSymbol);
     }
 
     const trunkWidth = 1;
-    result += buildLayer(width, trunkWidth, trunkSymbol, spaceSymbol, endlineSymbol);
+    result += buildLayer(width, trunkWidth, trunkSymbol);
 
     return result;
 }
