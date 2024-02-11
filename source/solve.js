0f6439ebf711904de868db0cf3b3710b6422fdbe
PetrMitin <petr09mitin@mail.ru>
2024-02-11 12:15:54 +0300

    Added type checking and more tests

--- source/solve.js
+++ source/solve.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const PLUS = '+'
-const MINUS = '-'
-const MULTIPLY = '*'
-const LEFT_PARENTH = '('
-const RIGHT_PARENTH = ')'
+const PLUS = '+';
+const MINUS = '-';
+const MULTIPLY = '*';
+const LEFT_PARENTH = '(';
+const RIGHT_PARENTH = ')';
 
 const OPERATORS_WITH_PRIORITY = {
     '+': 1,
     '-': 1,
     '*': 2,
-}
+};
 
 // Преобразует строковое арифметическое выражение в массив операндов, операторов и скобок.
 // Просто expression.split(' ') не подходит, так как есть случаи вида '(x + 1)', 
@@ -18,23 +18,33 @@ const OPERATORS_WITH_PRIORITY = {
 const parseOperandsFromExpression = (expression, x) => {
     const stringX = x.toString()
     let number = '';
+    let operatorsCount = 0;
+    let operandsCount = 0;
     let result = expression.split('').reduce((acc, curr) => {
         switch(curr) {
             case PLUS:
             case MINUS:
             case MULTIPLY:
+                ++operatorsCount;
             case LEFT_PARENTH:
             case RIGHT_PARENTH:
-                if (number) acc.push(number);
+                if (number) {
+                    acc.push(number);
+                    ++operandsCount;
+                }
                 number = '';
                 acc.push(curr);
                 break;
             case ' ':
-                if (number) acc.push(number);
+                if (number) {
+                    acc.push(number);
+                    ++operandsCount;
+                }
                 number = '';
                 break;
             case 'x':
                 acc.push(stringX);
+                ++operandsCount;
                 break;
             default:
                 number += curr;
@@ -43,7 +53,13 @@ const parseOperandsFromExpression = (expression, x) => {
         return acc;
     }, []);
 
-    if (number) result.push(number);
+    if (number) {
+        result.push(number);
+        ++operandsCount;
+    }
+
+    if (operandsCount !== operatorsCount + 1)
+        throw new Error(`invalid input expression ${expression}`);
 
     return result;
 }
@@ -60,9 +76,7 @@ const parseRPNFromExpression = (expression, x) => {
     const stack = [];
     const queue = [];
 
-    for (let i = 0; i < tokens.length; ++i) {
-        const token = tokens[i];
-        
+    tokens.forEach(token => {
         if (token.match(/\d+$/)) { // Если токен - число
             queue.push(token);
         } else if (Object.keys(OPERATORS_WITH_PRIORITY).includes(token)) { // Если токен - оператор
@@ -72,16 +86,20 @@ const parseRPNFromExpression = (expression, x) => {
             stack.push(token);
         } else if (token === LEFT_PARENTH) {
             stack.push(token);
-        } else if (token === RIGHT_PARENTH && stack.length > 0) {
+        } else if (token === RIGHT_PARENTH) {
             let lastLeftParenthIdx = stack.lastIndexOf(LEFT_PARENTH);
-            if (lastLeftParenthIdx === -1) throw new Error(`no left parenth in stack while parsing rpn from expression`);
-            queue.push(...stack.slice(lastLeftParenthIdx + 1).reverse());
+            if (stack.length === 0 || lastLeftParenthIdx === -1) 
+                throw new Error(`invalid parentheses sequence in input expression ${expression}`);
+            queue.push(...stack.splice(lastLeftParenthIdx + 1).reverse());
             stack.pop();
         } else {
             throw new Error(`invalid token ${token} while parsing rpn from expression`);
         }
-    }
-    
+    })
+
+    if (stack.indexOf(LEFT_PARENTH) !== -1) 
+        throw new Error(`invalid parentheses sequence in input expression ${expression}`);
+
     queue.push(...stack.reverse());
 
     return queue;
@@ -98,12 +116,19 @@ const evalToken = (operand1, operand2, operator) => {
         case MULTIPLY:
             return intOperand1 * intOperand2;
         default:
-            return NaN;
+            throw new Error(`invalid operator ${operator}`);
     }
 }
 
+const checkIfValidParamTypes = (expression, x) => (
+    (typeof expression === 'string' || typeof expression === 'symbol') 
+    && (typeof x === 'number' || typeof x === 'bigint')
+);
+
 const solve = (expression, x) => {
     try {
+        if (!checkIfValidParamTypes(expression, x)) 
+            throw new Error(`invalid type of some argument`);
         const queue = parseRPNFromExpression(expression, x);
         const stack = [];
 
@@ -118,6 +143,8 @@ const solve = (expression, x) => {
                 const operand2 = stack.pop();
                 const operand1 = stack.pop();
                 stack.push(evalToken(operand1, operand2, token));
+            } else {
+                throw new Error(`invalid token ${token} in rpn`);
             }
         });
 
--- tests/solve.js
+++ tests/solve.js
@@ -10,8 +10,19 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
 	});
 
-	QUnit.test('solve возвращает NaN при вводе некорректного выражения ', function (assert) {
-		assert.notOk(solve('x / 0', 1));
-		assert.notOk(solve('a * 1', 2));
+	QUnit.test('solve возвращает NaN при вводе некорректного выражения', function (assert) {
+		assert.true(isNaN(solve('x / 0', 1)));
+		assert.true(isNaN(solve('a * 1', 2)));
+		assert.true(isNaN(solve('x + 1)', 1)));
+		assert.true(isNaN(solve('(x + 1', 1)));
+		assert.true(isNaN(solve('x a 1', 1)));
+		assert.true(isNaN(solve('x123 + 1', 1)));
+		assert.true(isNaN(solve('123 1', 1)));
+		assert.true(isNaN(solve('', 1)));
 	});
+
+	QUnit.test('solve возвращает NaN при неверном типе хотя бы одного аргумента', function (assert) {
+		assert.true(isNaN(solve([], 1)));
+		assert.true(isNaN(solve('x', '')));
+	})
 });
