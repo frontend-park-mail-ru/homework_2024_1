447a5216656ae416086b780611019a608d3f2a28
FunnyRockfish <123645724+FunnyRockfish@users.noreply.github.com>
2024-02-12 01:52:32 +0300

    Update filter.js

--- tests/filter.js
+++ tests/filter.js
@@ -25,4 +25,42 @@ QUnit.module('Проверка работы функции filter', function () 
 		assert.strictEqual(filter(`<script>alert('1');</script>`, [ 'strong', 'em' ]), '&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;');
 		assert.strictEqual(filter(`<img src="bad" onerror="alert('1');">`, [ 'strong', 'em' ]), '&lt;img src=&quot;bad&quot; onerror=&quot;alert(&#39;1&#39;);&quot;&gt;');
 	});
+	QUnit.test('filter экранирует специальные символы HTML', function (assert) {
+    const input = '<>&"\'';
+
+    const output = filter(input, ['strong', 'em']);
+
+    const expected = '&lt;&gt;&amp;&quot;&#39;';
+
+    assert.strictEqual(output, expected);
+});
+
+QUnit.test('filter экранирует HTML теги, когда они не разрешены', function (assert) {
+    const input = '<script>alert("XSS");</script>';
+
+    const output = filter(input, ['strong', 'em']);
+
+    const expected = '&lt;script&gt;alert(&quot;XSS&quot;);&lt;/script&gt;';
+
+    assert.strictEqual(output, expected);
+});
+
+QUnit.test('filter корректно обрабатывает пустой ввод', function (assert) {
+    const input = '';
+
+    const output = filter(input, ['strong', 'em']);
+
+    const expected = '';
+
+    assert.strictEqual(output, expected);
+});
+QUnit.test('filter не изменяет текст, если все теги разрешены', function (assert) {
+    const input = '<div>Hello, <em>World!</em></div>';
+
+    const output = filter(input, ['div', 'em']);
+
+    const expected = '<div>Hello, <em>World!</em></div>';
+
+    assert.strictEqual(output, expected);
+});
 });
