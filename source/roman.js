a232247c16913a5b348428f18b630ba474f35bef
dozerdozerdozer <142627025+dozerdozerdozer@users.noreply.github.com>
2024-02-11 23:16:30 +0300

    Update roman.js

--- source/roman.js
+++ source/roman.js
@@ -1,17 +1,26 @@
 'use strict';
-
+/**
+ * Преобразует римские числа в арабские и наоборот.
+ * @param {string|number} roman - Если передается строка, то она рассматривается как римское число, если число, то как арабское.
+ * @returns {string|number} - Возвращает строку  римское число, если вход был арабским числом, или число, если вход был римским числом.
+ */
 const roman = (roman) => {
+    /**
+     * Массив объектов, содержащий пары римских чисел и их соответствующих арабских значений.
+     * @type {Array<{num: number, ch: string}>}
+     */
     const romanNumbers = [
         {num: 1, ch: "I"}, {num: 4, ch: "IV"}, {num: 5, ch: "V"},
         {num: 9, ch: "IX"}, {num: 10, ch: "X"}, {num: 40, ch: "XL"},
         {num: 50, ch: "L"}, {num: 90, ch: "XC"}, {num: 100, ch: "C"},
         {num: 400, ch: "CD"}, {num: 500, ch: "D"}, {num: 900, ch: "CM"},
-        {num: 1000, ch: "M"},
+        {num: 1000, ch: "M"}, {num: 4000, ch: "MIƆƆƆ"}, {num: 5000, ch: "IƆƆƆ"},
     ];
 
     if (isNaN(roman)) {
         roman = roman.toUpperCase();
-        let idx = 12;
+        console.log(roman)
+        let idx = 14;
         let pointer = 0;
         let result = 0;
         while (pointer < roman.length) {
@@ -24,9 +33,14 @@ const roman = (roman) => {
         }
         return result;
     } else {
-        let pointer = 12;
+        let pointer = 14;
         let result = "";
-        let A = roman;
+        let A;
+        if (typeof(roman) === "string") {
+            A = roman.replace(" ", "").replace(".", "");
+        } else {
+            A = roman;
+        }
         while (A > 0) {
             while (romanNumbers[pointer].num > A) {
                 pointer -= 1;
@@ -38,3 +52,6 @@ const roman = (roman) => {
     }
 }
 
+console.log(isNaN("boba"))
+console.log(isNaN("1904"))
+console.log(isNaN(1904))
