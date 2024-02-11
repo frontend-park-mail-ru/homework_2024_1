791947b3afe01abcc5639664922208177190fbd0
smail <smailcmail@gmail.com>
2024-02-12 00:28:27 +0300

    for -> forEach .reduce added typeof checking added "()" and "{}" added

--- source/letters.js
+++ source/letters.js
@@ -1,4 +1,4 @@
-'use strict'; // typescript на минималках
+'use strict';
 
 /**
  * Функция удаляет из строки символы, которые встречаются в ней больше одного раза.
@@ -7,23 +7,24 @@
  * @returns {string} Конечный результат.
  */
 const letters = (text, flag) => {
+    if (typeof text !== "string")
+        throw new Error("Аргумент text обязательно должен быть типа string")
+
     // Map с частотным анализом
-    const map = new Map();
-    for (const letter of text)
-        if (map.has(letter))
-            map.set(letter, map.get(letter)+1);
-        else
-            map.set(letter, 1);
+    const map = Array.from(text).reduce(
+        (m, letter) => m.set(letter, (m.get(letter) || 0) + 1),
+        new Map()
+    );
 
     // Повторно обходим данную строку и выдаём результат
     let res = "";
-    for (const letter of text)
-        if (map.get(letter) === 1 || map.get(letter) !== 0 && flag){
+    Array.from(text).forEach(letter => {
+        if (map.get(letter) === 1 || (map.get(letter) !== 0 && flag)){
             res += letter;
             map.set(letter, 0);
-        }
-        else if (flag === false)
+        } else if (flag === false) {
             map.set(letter, map.get(letter)-1);
-
+        }
+    })
     return res;
 };
