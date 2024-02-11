90666915d430f1cd5fed3a34d4739e60bca387a2
novakf <bogdannovikov116@mail.ru>
2024-02-11 22:31:01 +0300

    add own sort function

--- source/sort.js
+++ source/sort.js
@@ -6,14 +6,34 @@
  * @returns {string} - sorted sentence with sorted words
  */
 
-const sort = (sentence) => sentence.split(' ').map(wordSort).sort(ruComparator).join(' ');
+const sort = (sentence) =>
+  quickSort(sentence.split(' ').map(wordSort)).join(' ');
 
 const wordSort = (word) => {
-  word = word.split('').sort(ruComparator).join('');
+  word = quickSort(word.split('')).join('');
   return word[0].toUpperCase() + word.slice(1).toLowerCase();
 };
 
 const ruComparator = (a, b) => {
   const collator = new Intl.Collator('ru-RU');
   return collator.compare(a, b);
+};
+
+const quickSort = (arr) => {
+  if (arr.length < 2) return arr;
+
+  let leftArr = [];
+  let rightArr = [];
+  
+  let pivot = arr[0];
+
+  arr.slice(1).forEach((val) => {
+    if (ruComparator(val, pivot) === -1) leftArr.push(val);
+    else rightArr.push(val);
+  });
+
+  let sortLeft = quickSort(leftArr);
+  let sortRight = quickSort(rightArr);
+
+  return [...sortLeft, pivot, ...sortRight];
 };
