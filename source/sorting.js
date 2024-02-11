52fdf670c54be60e1d45cb4354d2bebe957dc427
glebkostin <glebkos777@gmail.com>
2024-02-11 21:29:37 +0300

    Убрал ненужное

--- source/sorting.js
+++ source/sorting.js
@@ -1,8 +1,7 @@
-'using strict';
+'use strict';
 
 /**
  * Сравнивает переменные, если они строки, то лексиграфически, иначе переводит в тип Number
- * @constructor
  * @param {any} a 
  * @param {any} b 
  * @returns {number} Если a > b возвращает положительное число, если a === b, то 0, иначе отрицательное число
@@ -17,12 +16,11 @@ function comporator(a, b){
 
 /**
  * Сортирует массив по убыванию
- * @constructor
  * @param {Array} array - массив plain-объектов
  * @param {Array} keys - массив ключей, по которым нужно отсортировать
  * @returns {Array} отсортированный по убыванию массив
  */
-let sorting = (array, keys) => {
+const sorting = (array, keys) => {
     for (let key of keys.reverse()) {
         for (let [index, item] of array.entries()) {
             let insertIndex = array.findIndex((elem) => (comporator(elem[key], item[key]) > 0));
