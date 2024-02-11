d6f36f3f4144e5e34fc6f983e82be506971f479c
DeadBread-001 <nikita10062004m@gmail.com>
2024-02-11 13:12:28 +0300

    optimization, fixes + throw errors

--- source/euclid.js
+++ source/euclid.js
@@ -4,23 +4,18 @@
 
 const greatestCommonDivisor = (a, b) => (b === 0 ? a : greatestCommonDivisor(b, a % b));
 
+const validateNaturalNumbers = numbers => {
+    return numbers.every(number => Number.isInteger(number) && number > 0);
+}
+
 const euclid = (...numbers) => {
-    if (numbers.some(isNaN)) {
-        return 'Все элементы должны быть натуральными числами.';
+    if (!validateNaturalNumbers(numbers)) {
+        throw new Error("Все числа должны быть натуральными.");
     }
 
-    if (numbers.some(number => !Number.isInteger(number) || number <= 0)) {
-        return 'Все числа должны быть натуральными.';
-    }
-
-    if (numbers.length < 2) {
+    if (numbers.length === 1) {
         return numbers[0];
     }
 
-    let result = numbers[0];
-    numbers.forEach(number => {
-        result = greatestCommonDivisor(result, number);
-    });
-
-    return result;
+    return numbers.reduce(greatestCommonDivisor);
 };
