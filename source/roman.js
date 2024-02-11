e192236364d543aade9f5f538a3c56be88aeff99
dozerdozerdozer <142627025+dozerdozerdozer@users.noreply.github.com>
2024-02-11 23:43:26 +0300

    Update roman.js

--- source/roman.js
+++ source/roman.js
@@ -17,40 +17,44 @@ const roman = (roman) => {
         {num: 1000, ch: "M"}, {num: 4000, ch: "MIƆƆƆ"}, {num: 5000, ch: "IƆƆƆ"},
     ];
 
-    if (isNaN(roman)) {
-        roman = roman.toUpperCase();
-        if (/^[IVXLCDM]+$/i.test(roman)) {
-            let idx = 14;
-            let pointer = 0;
-            let result = 0;
-            while (pointer < roman.length) {
-                while (roman.substring(pointer, pointer + romanNumbers[idx].ch.length) !== romanNumbers[idx].ch) {
-                    idx--;
-                    if (idx === 0) break;
+    if (typeof (roman) !== "number" && typeof (roman) !== "string") {
+        return null;
+    } else {
+        if (isNaN(roman)) {
+            roman = roman.toUpperCase();
+            if (/^[IVXLCDM]+$/i.test(roman)) {
+                let idx = 14;
+                let pointer = 0;
+                let result = 0;
+                while (pointer < roman.length) {
+                    while (roman.substring(pointer, pointer + romanNumbers[idx].ch.length) !== romanNumbers[idx].ch) {
+                        idx--;
+                        if (idx === 0) break;
+                    }
+                    result += romanNumbers[idx].num;
+                    pointer += romanNumbers[idx].ch.length;
                 }
-                result += romanNumbers[idx].num;
-                pointer += romanNumbers[idx].ch.length;
+                return result;
+            } else {
+                return null;
+            }
+        } else {
+            let pointer = 14;
+            let result = "";
+            let A;
+            if (typeof (roman) === "string") {
+                A = roman.replace(" ", "").replace(".", "");
+            } else {
+                A = roman;
+            }
+            while (A > 0) {
+                while (romanNumbers[pointer].num > A) {
+                    pointer -= 1;
+                }
+                result += romanNumbers[pointer].ch;
+                A -= romanNumbers[pointer].num;
             }
             return result;
-        } else {
-            return null;
         }
-    } else {
-        let pointer = 14;
-        let result = "";
-        let A;
-        if (typeof (roman) === "string") {
-            A = roman.replace(" ", "").replace(".", "");
-        } else {
-            A = roman;
-        }
-        while (A > 0) {
-            while (romanNumbers[pointer].num > A) {
-                pointer -= 1;
-            }
-            result += romanNumbers[pointer].ch;
-            A -= romanNumbers[pointer].num;
-        }
-        return result;
     }
 }
