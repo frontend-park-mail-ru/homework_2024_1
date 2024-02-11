da17945f749b651260e0d3a9468592e3bf439d3a
Gavriil <gavriil.belov@yandex.ru>
2024-02-12 00:38:22 +0300

    Small fixes.

--- source/plainify.js
+++ source/plainify.js
@@ -3,6 +3,7 @@
 /*
  * Returns plained object by given object. Throws Error if argument is not an object.
  * @function
+ * @name plainify
  * @throws {InvalidArgumentException} If argument is not an object-type.
  * @param {object} object: Object to plainify.
  * @returns {object} object: Plained object.
@@ -17,11 +18,9 @@ const plainify = function (object) {
 const plain_obj = function (object, parent_string="") {
 	let result = [];
 	if (parent_string != "") { parent_string += "."}
-	for (let key in object) {
+	for (const key in object) {
 		if (Object.prototype.hasOwnProperty.call(object, key)) {
-			console.log(key);
-			console.log("KEY UP");
-			name = parent_string + key;
+			const name = parent_string + key;
 			if (typeof object[key] != 'object' || object[key] === null) {
 				result.push([name, object[key]]);
 			} 
