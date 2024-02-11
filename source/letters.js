24237e53a7e514d97656eb7f20b298b340165620
Bopster411 <serg.dr2004@gmail.com>
2024-02-11 09:39:37 +0300

    Use forEach method now

--- source/letters.js
+++ source/letters.js
@@ -1,8 +1,8 @@
 function letters(seq, saveFirst) {
+    let seqArr = Array.from(seq)
     let seen = new Map();
     // Analyse sequence
-    for (let i = 0; i < seq.length; i++) {
-        let letter = seq[i];
+    seqArr.forEach(function(letter, i) {
         if (seen.has(letter)) {
             seen.get(letter).last = i;
             seen.get(letter).unique = false;
@@ -12,18 +12,17 @@ function letters(seq, saveFirst) {
             // unique - is letter unique in sequence
             seen.set(letter, {first: i, last: i, unique: true});
         }
-    }
+    })
 
     // Create new sequence
     newSeq = "";
-    for (let i = 0; i < seq.length; i++) {
-        let letter = seq[i];
+    seqArr.forEach(function(letter, i) {
         let letterObj = seen.get(letter);
         // "saveFirst == false" is due to there is an "saveFirst == undefined" option possible
         if (letterObj.unique || (saveFirst && i == letterObj.first) || (saveFirst == false && i == letterObj.last)) {
             newSeq += letter;
         }
-    }
+    })
 
     return newSeq;
 }
