5280ce3dc64f662127baccf692ed5f19c7eaf89a
fztott <anton.zhidkov.01@mail.ru>
2024-02-11 23:22:45 +0300

    допустил плохую практку

--- source/minmax.js
+++ source/minmax.js
@@ -6,10 +6,10 @@ const minmax = numericString => {
 
 	}
 
-    subStringArray = numericString.split(' ').filter(num => num !== '');
+    const subStringArray = numericString.split(' ').filter(num => num !== '');
 
 	//преобразование подстрок в значение
-	numArray = subStringArray.map(Number).filter(num => !isNaN(num));
+	const numArray = subStringArray.map(Number).filter(num => !isNaN(num));
 	if (numArray.length === 0) {
 
 		return [undefined, undefined];
