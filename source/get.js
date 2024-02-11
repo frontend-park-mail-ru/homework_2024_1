d337897dc12f293c6fcc3877154540ba5de7abcd
EvilAv <andrey.vookolov85@gmail.com>
2024-02-11 23:50:57 +0300

    add jsdoc

--- source/get.js
+++ source/get.js
@@ -1,5 +1,12 @@
 'use strict'
 
+/**
+ * Return value of property by given property name, return undefined in any error case.
+ * @function
+ * @param {object} obj - Object where function searches value of property.
+ * @param {string} property - Property name.
+ * @returns {object} 
+ */
 const get = (obj, property) => {
     if (typeof(property) !== 'string' && !(property instanceof String)) return undefined;
     const propertyQuery = property.split('.').filter(str => str.length > 0);
