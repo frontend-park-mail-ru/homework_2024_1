5bdf62d9a3674a8819f28c1417dd88c2838baac8
Dima <farset007@mail.ru>
2024-02-11 21:32:12 +0300

    style: add arrow funcs + jsDoc

--- source/euclid.js
+++ source/euclid.js
@@ -1,18 +1,33 @@
 'use strict';
 
-function gcd(num1, num2) {
-    if (num2 == 0)
-        return num1;
+/**
+ * Returns greatest common diviser of two numbers
+ * @param {Number} num1
+ * @param {Number} num2
+ * @returns {Number} gcd
+ */
+const gcd = (num1, num2) => {
+    if (!num2)
+    return num1;
 
     return gcd(num2, num1 % num2);
 }
 
-function euclid(...numbers) {
-    let result = numbers[0];
-
-    for (let i = 1; i < numbers.length; i++) {
-        result = gcd(result, numbers[i]);
+/**
+ * Returns greatest common diviser of many numbers
+ * @param  {...Number} numbers
+ * @returns {Number} gcd of all numbers
+ */
+const euclid = (...numbers) => {
+    if (!numbers.length) {
+        return undefined
     }
+
+    let result = numbers[0];
+    
+    numbers.forEach((number) => {
+        result = gcd(result, number)
+    })
 
     return result;
 }
