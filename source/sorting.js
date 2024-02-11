574272ba913010594b810c62f9a080c9625c3158
glebkostin <glebkos777@gmail.com>
2024-02-11 19:15:25 +0300

    Реализовал свой sort

--- source/sorting.js
+++ source/sorting.js
@@ -1,8 +1,36 @@
-"using strict";
+'using strict';
 
-let sorting = (array, keys) => {
-    for (item of keys.reverse()) {
-        array.sort((a, b) => (typeof a[item] === 'string') && (typeof b[item] === 'string') ? a[item].localeCompare(b[item]) : a[item] - b[item]);
+/**
+ * Сравнивает переменные, если они строки, то лексиграфически, иначе переводит в тип Number
+ * @constructor
+ * @param {any} a 
+ * @param {any} b 
+ * @returns {number} Если a > b возвращает положительное число, если a === b, то 0, иначе отрицательное число
+ */
+function comporator(a, b){
+    if ((typeof a === 'string') && (typeof b === 'string')) {
+        return a.localeCompare(b);
+    } else {
+        return a - b;
     }
-    return array
+}
+
+/**
+ * Сортирует массив по убыванию
+ * @constructor
+ * @param {Array} array - массив plain-объектов
+ * @param {Array} keys - массив ключей, по которым нужно отсортировать
+ * @returns {Array} отсортированный по убыванию массив
+ */
+let sorting = (array, keys) => {
+    for (let key of keys.reverse()) {
+        for (let [index, item] of array.entries()) {
+            let insertIndex = array.findIndex((elem) => (comporator(elem[key], item[key]) > 0));
+            if ((insertIndex >= 0) && (insertIndex < index)){
+                array.splice(index, 1);
+                array.splice(insertIndex, 0, item);
+            }
+        }
+    }
+    return array;
 };
