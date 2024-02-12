22f9ddae0407aaab21b08f7d8ef619ba0180ac19
Artem Zhuk <artem.zx656@gmail.com>
2024-02-12 20:48:33 +0300

    added jsdoc

--- source/get.js
+++ source/get.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+* Получает путь к вложенному свойству объекта и возвращает значение этого свойства (или undefined, если свойства не существует)
+* @param {object} object - Объект.
+* @param {string} property - свойство объекта.
+* @return {object|undefined} - Возвращает значение или undefined.
+*/
 function get(object, property){
     property = property.split('.');
     property.splice(0,1);
