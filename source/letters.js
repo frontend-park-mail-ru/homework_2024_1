b8ef0aef97f4214cb0117900f611dde98db5f628
smail <smailcmail@gmail.com>
2024-02-12 19:20:29 +0300

    some improvements

--- source/letters.js
+++ source/letters.js
@@ -1,6 +1,48 @@
 'use strict';
 
 /**
+ * Функция составляет Map с частотным анализом каждого элемента входного массива.
+ * @param {Array} arr Входной массив.
+ * @returns {Map} Map, где ключ - элемент, значение - его частота.
+ */
+const freqMap = (arr) => {
+    // в рамках этой задачи вместо array можно юзать string, но пускай функция будет более универсальной
+    if (!(arr instanceof Array)) {
+        throw new Error("Функция работает только с массивами");
+    }
+    return arr.reduce(
+        (m, elem) => m.set(elem, (m.get(elem) || 0) + 1),
+        new Map()
+    );
+}
+
+/**
+ * Функция удаляет из массива элементы, которые встречаются более одного раза.
+ * @param {Array} arr Входной массив.
+ * @param {Map} freq Map с частотным анализом элементов.
+ * @param {boolean} flag Если true, то оставляет первый элемент среди повторяющихся. Если false, то последний. Если аргумент не указан, то удаляет все повторяющиеся элементы.
+ * @returns {Array} Конечный результат.
+ */
+const arrayByFreq = (arr, freq, flag) => {
+    if (!(arr instanceof Array)) {
+        throw new Error("Функция работает только с массивами");
+    }
+    return arr.reduce(
+        (acc, elem) => {
+            if (freq.get(elem) === 1 || (freq.get(elem) !== 0 && flag)){
+                freq.set(elem, 0);
+                return acc + [elem];
+            }
+            if (flag === false) {
+                freq.set(elem, freq.get(elem)-1);
+            }
+            return acc;
+        },
+        []
+    );
+}
+
+/**
  * Функция удаляет из строки символы, которые встречаются в ней больше одного раза.
  * @param {string} text Строка.
  * @param {boolean} flag Если true, то оставляет первый символ среди повторяющихся. Если false, то последний. Если аргумент не указан, то удаляет все повторяющиеся символы.
@@ -10,24 +52,5 @@ const letters = (text, flag) => {
     if (typeof text !== "string") {
         throw new Error("Аргумент text обязательно должен быть типа string")
     }
-
-    // Map с частотным анализом
-    const map = Array.from(text).reduce(
-        (m, letter) => m.set(letter, (m.get(letter) || 0) + 1),
-        new Map()
-    );
-
-    // Повторно обходим данную строку и выдаём результат
-    return Array.from(text).reduce(
-        (res, letter) => {
-            if (map.get(letter) === 1 || (map.get(letter) !== 0 && flag)){
-                map.set(letter, 0);
-                return res + letter;
-            } else if (flag === false) {
-                map.set(letter, map.get(letter)-1);
-            }
-            return res;
-        },
-        ""
-    );
+    return arrayByFreq(Array.from(text), freqMap(Array.from(text)), flag).toString();
 };
