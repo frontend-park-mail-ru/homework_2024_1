4c46168a2a3c1aa9fbdb7dcb0f95c94d9f6ffc54
fztott <anton.zhidkov.01@mail.ru>
2024-02-12 23:23:15 +0300

    вернул файлы

--- source/minmax.js
+++ source/minmax.js
@@ -1,24 +1,20 @@
 const minmax = numericString => {
-	
 	if (typeof numericString !== typeof ' '){
 
 		return [undefined, undefined];
-
 	}
 
-    const subStringArray = numericString.split(' ').filter(num => num !== '');
+	const subStringArray = numericString.split(' ').filter(num => num !== '');
 
 	//преобразование подстрок в значение
 	const numArray = subStringArray.map(Number).filter(num => !isNaN(num));
 	if (numArray.length === 0) {
 
 		return [undefined, undefined];
-
 	}
 
     const min = Math.min(...numArray);
     const max = Math.max(...numArray);
 
     return [min, max];
-
 };
