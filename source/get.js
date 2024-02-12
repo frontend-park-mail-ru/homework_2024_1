3fbb26a1ab2d8a0501ff0996f706d5f1ab8096fa
Artem Zhuk <artem.zx656@gmail.com>
2024-02-12 23:45:18 +0300

    changed var to let

--- source/get.js
+++ source/get.js
@@ -9,8 +9,8 @@
 function get(object, property){
     property = property.split('.');
     property.splice(0,1);
-    var current = object;
-    for (var i = 0; i < property.length && property[i] != "" && current !== undefined; i++){
+    let current = object;
+    for (let i = 0; i < property.length && property[i] != "" && current !== undefined; i++){
         current = current[property[i]];
     }
     return current;
