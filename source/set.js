d53cf0781a80537cc6e6ae3ebad89e615f521d0b
Alexey Khochenkov <alekshoch@yandex.ru>
2024-02-12 23:42:23 +0300

    Fixed problems, implemented arrow function, added JSDoc

--- source/set.js
+++ source/set.js
@@ -1,20 +1,27 @@
 'use strict';
 
-function set(objName={}, path, value){
-    let currentPos=objName;
-    let currentPath=[];
-    let attributes = path.slice(1).split('.');
-    for (let i=0; i < attributes.length; i++){
-        if (Object.keys(currentPos).length===0){
-            currentPos[attributes[i]]={};
+/**
+ * Function, that sets value for given attribute. Returns modified object.
+ * @param {object} objName - reference to the object we need to modify.
+ * @param {string} path - string, that contains path of attributes of the object to the exact attribute we need to change.
+ * @param {*} value - value, that we need to set.
+ */
+const set = (objName = {}, path, value) => {
+    let currentPos = objName;
+    const currentPath = [];
+    const attributes = path.slice(1).split('.');
+    attributes.forEach((attribute, i) => {
+        if (Object.keys(currentPos).length === 0){
+            currentPos[attribute] = {};
         }
-        currentPath[i]=currentPos;
-        currentPos=currentPos[attributes[i]];
-    }
-    currentPos=value;
-    for (let i=currentPath.length-1; i>=0; i--){
-        currentPath[i][attributes[i]]=currentPos;
-        currentPos=currentPath[i];
-    }
+        currentPath[i] = currentPos;
+        currentPos = currentPos[attribute];
+    });
+    currentPos = value;
+    attributes.reverse();
+    currentPath.reverse().forEach((pathElement, i) => {
+        pathElement[attributes[i]] = currentPos;
+        currentPos = pathElement;
+    });
     return currentPos;
 }
