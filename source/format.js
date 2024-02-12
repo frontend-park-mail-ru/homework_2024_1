379cd677baf0919d74e55aaaf4c5cb647a526af8
Ilya <ilya.kovalev001@gmail.com>
2024-02-13 01:33:02 +0300

    [*]: refactor according to review

--- source/format.js
+++ source/format.js
@@ -1,23 +1,42 @@
-'use strict'
+'use strict';
 
-const column = function (numbers, columns, nCol) {
-    let numbersInColumn = []
-    for (let i = 0; i < numbers.length; i++) {
+/**
+ * Нахождение чисел из одного столбца
+ * @param {number[]} numbers - Все числа
+ * @param {number} columns - Кол-во колонок
+ * @param {number} nCol - Номер нужной колонки
+ * @returns {number[]} Числа из nCol-й колонки
+ */
+const column = (numbers, columns, nCol) => {
+    return numbers.reduce((columnNumbers, num, i) => {
         if (i % columns === nCol) {
-            numbersInColumn.push(numbers[i])
+            columnNumbers.push(num);
         }
+        return columnNumbers;
+    }, []);
+};
+
+/**
+ * Форматирование строки
+ * @param {number[]} numbers - Массив чисел
+ * @param {number} col - Кол-во колонок для вывода чисел
+ * @returns {string} Отформатированная строка с числами переданного массива
+ */
+const format = (numbers, col) => {
+    if (!Array.isArray(numbers) || col <= 0) {
+        return '';
     }
-    return numbersInColumn
-}
-  
-const format = function (numbers, col) {
-    let result = ''
-    for (let i = 0; i < numbers.length; i++) {
-        const maxLength = Math.max(...column(numbers, col, i % col).map(num => String(num)).map(str => str.length));
-        result += ' '.repeat(maxLength - String(numbers[i]).length + 1) + numbers[i]
-        if ((i + 1) % col === 0 && i != numbers.length - 1) {
-            result += '\n'
+    return numbers.reduce((result, num, i) => {
+        const columnNumbers = column(numbers, col, i % col);
+        const columnOfStrings = columnNumbers.map(n => String(n));
+        const arrayOfLength = columnOfStrings.map(str => str.length);
+        const maxLength = Math.max(...arrayOfLength);
+        const spacesForAlign = maxLength - String(numbers[i]).length;
+        result += ' '.repeat(spacesForAlign + ((i % col) === 0 ? 0 : 1)) + numbers[i];
+        let isLastRow;
+        if ((i + 1) % col === 0 && (isLastRow = i) != numbers.length - 1) {
+            result += '\n';
         }
-    }
-    return result
-} ;
+        return result;
+    }, []);
+};
