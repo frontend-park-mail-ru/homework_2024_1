4a289ad00618f9e62571be7e9d04db4473adae8f
Artem Zhuk <artem.zx656@gmail.com>
2024-02-13 19:52:50 +0300

    changed splice to slice

--- source/get.js
+++ source/get.js
@@ -7,8 +7,7 @@
  * @return {Object|undefined} - Возвращает значение или undefined.
  */
 const get = (object, property) => {
-    let propertyArray = property.split('.');
-    propertyArray = propertyArray.splice(0, 1).filter((item) => item.length > 0);
+    let propertyArray = property.split('.').slice(1).filter(item => item.length > 0);
     let current = object;
     for (let i = 0; i < propertyArray.length; i++) {
         if (!current.hasOwnProperty(propertyArray[i])) {
