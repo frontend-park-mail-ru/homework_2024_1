d2276be3d3c5f6da54dcf22e917ee4fb0e5a9add
Ilya <ilya.kovalev001@gmail.com>
2024-02-13 21:39:41 +0300

    [*]: error handling + refactor

--- source/format.js
+++ source/format.js
@@ -19,27 +19,24 @@ const column = (numbers, columns, nCol) =>
  * Форматирование строки
  * @param {number[]} numbers - Массив чисел
  * @param {number} col - Кол-во колонок для вывода чисел
+ * @throws {TypeError} - Переданы невалидные параметры
  * @returns {string} Отформатированная строка с числами переданного массива
  */
 const format = (numbers, col) => {
     if (!Array.isArray(numbers) || col <= 0) {
-        return '';
+        throw new TypeError('wrong parameters');
     }
-    try {
-        return numbers.reduce((result, num, i) => {
-            const columnNumbers = column(numbers, col, i % col);
-            const columnOfStrings = columnNumbers.map(n => String(n));
-            const arrayOfLength = columnOfStrings.map(str => str.length);
-            const maxLength = Math.max(...arrayOfLength);
-            const spacesForAlign = maxLength - String(numbers[i]).length;
-            result += ' '.repeat(spacesForAlign + ((i % col) === 0 ? 0 : 1)) + numbers[i];
-            let isLastRow = (i == numbers.length - 1);
-            if ((i + 1) % col === 0 && !isLastRow) {
-                result += '\n';
-            }
-            return result;
-        }, '');
-    } catch(e) {
-        return '';
-    }
+    return numbers.reduce((result, num, i) => {
+        const columnNumbers = column(numbers, col, i % col);
+        const columnOfStrings = columnNumbers.map(n => String(n));
+        const arrayOfLength = columnOfStrings.map(str => str.length);
+        const maxLength = Math.max(...arrayOfLength);
+        const spacesForAlign = maxLength - String(numbers[i]).length;
+        result += ' '.repeat(spacesForAlign + ((i % col) === 0 ? 0 : 1)) + numbers[i];
+        const isLastRow = (i == numbers.length - 1);
+        if ((i + 1) % col === 0 && !isLastRow) {
+            result += '\n';
+        }
+        return result;
+    }, '');
 };
