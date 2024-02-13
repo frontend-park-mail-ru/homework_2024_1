b87729f5b80e14480ae6e7bd78384db7a28de451
AlexeiLDD <doroga218@gmail.com>
2024-02-13 18:16:31 +0300

    Added function zip and tests

--- source/zip.js
+++ source/zip.js
@@ -1,3 +1,20 @@
 'use strict';
 
-const zip = (...objects) => Object.assign({}, ...objects.reverse());
+/**
+ * Функция, возвращающая единственный объект, содержащий все поля из всех переданных объектов
+ * @param  {...object} objects 
+ * @returns {object}
+ */
+const zip = (...objects) => {
+    let resultObj = {};
+
+    for (const object of objects) {
+        for (const property in object) {
+            if (!resultObj.hasOwnProperty(property)) {
+                resultObj[property] = object[property];
+            }
+        }
+    }
+
+    return resultObj;
+}
