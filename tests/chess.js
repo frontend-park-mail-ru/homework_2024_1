0e14936d6465aa034a14386ef325c0dac4940bcb
Famelbyby <lexagorbunov14@gmail.com>
2024-02-14 02:00:47 +0300

    Add all types tests

--- tests/chess.js
+++ tests/chess.js
@@ -32,6 +32,23 @@ QUnit.module('Тестируем функцию chess', function () {
 		assert.strictEqual(chess('asd'), null);
 	});
 
+	QUnit.test('Шахматной доски [1, 2, 3] на [1, 2, 3] не бывает', function (assert) {
+		assert.strictEqual(chess([1, 2, 3]), null);
+	});
+
+	QUnit.test('Шахматной доски {name: "Alex"} на {name: "Alex"} не бывает', function (assert) {
+		assert.strictEqual(chess({name: "Alex"}), null);
+	});
+
+	QUnit.test('Шахматной доски boolean на boolean не бывает', function (assert) {
+		assert.strictEqual(chess(true), null);
+		assert.strictEqual(chess(false), null);
+	});
+
+	QUnit.test('Шахматной доски c отсутствующем значением длины доски не бывает', function (assert) {
+		assert.strictEqual(chess(null), null);
+		assert.strictEqual(chess(undefined), null);
+	});
 
 	QUnit.test('Шахматная доска 8 на 8', function (assert) {
 		const expected =
