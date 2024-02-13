c12c80219799ce20e409ce701acbc9100ad5cbcf
Artem Zhuk <artem.zx656@gmail.com>
2024-02-13 19:19:34 +0300

    refactored to arrow function

--- source/get.js
+++ source/get.js
@@ -1,18 +1,21 @@
 'use strict';
 
 /**
-* Получает путь к вложенному свойству объекта и возвращает значение этого свойства (или undefined, если свойства не существует)
-* @param {object} object - Объект.
-* @param {string} property - свойство объекта.
-* @return {object|undefined} - Возвращает значение или undefined.
-*/
-function get(object, property){
-    property = property.split('.');
-    property.splice(0,1);
+ * Получает путь к вложенному свойству объекта и возвращает значение этого свойства (или undefined, если свойства не существует)
+ * @param {Object} object - Объект.
+ * @param {string} property - свойство объекта.
+ * @return {Object|undefined} - Возвращает значение или undefined.
+ */
+const get = (object, property) => {
+    let propertyArray = property.split('.');
+    propertyArray = propertyArray.splice(0, 1).filter((item) => item.length > 0);
     let current = object;
-    for (let i = 0; i < property.length && property[i] != "" && current !== undefined; i++){
-        current = current[property[i]];
+    for (let i = 0; i < propertyArray.length; i++) {
+        if (!current.hasOwnProperty(propertyArray[i])) {
+            current = undefined;
+            break;
+        }
+        current = current[propertyArray[i]];
     }
     return current;
 }
-
