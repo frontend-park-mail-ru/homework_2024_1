8b746a8c02cfe3d86e8dc5f983166b0146068f1d
Oleg Zhdanov <helge2003@yandex.ru>
2024-02-14 21:43:31 +0300

    fixed style

--- source/rle.js
+++ source/rle.js
@@ -1,11 +1,15 @@
+const isString = (obj) => {
+    return typeof obj === 'string' || obj instanceof String; 
+}
+
 const rle = (input) => {
-    if (!(typeof input === 'string' || input instanceof String)) {
+    if (!isString(input)) {
 	throw new Error('Parameter is not a string or missing!');
     }
     let result = '';
     let count = 1;
     for (let i = 1; i <= input.length; i++) {
-	if (input[i] === input[i-1]) {
+	if (input[i] === input[i - 1]) {
 	    count++;
 	} else {
 	    result += input[i-1];
@@ -15,5 +19,7 @@ const rle = (input) => {
 	    count = 1;
 	}
     }
+
     return result;
 }
+
