9ac527e28679108ff443fed00bf6b9d851a595f0
MrDzhofik <djafarovemil04@mail.ru>
2024-02-15 00:24:17 +0300

    Code is getting more beatufil

--- source/sort.js
+++ source/sort.js
@@ -1,11 +1,25 @@
 'use strict'
 
-function sortLetters(word) {
-    word = word.toLowerCase().split('').sort((letter, bukva) => letter.localeCompare(bukva)).join(''); // сортируем буквы в слове
-    return word[0].toUpperCase() + word.slice(1);
+/* Сортировка букв в слове */
+const sortLetters = (word) => {
+    const newWord = word
+        .toLowerCase()
+        .split('')
+        .sort((letter, char) => letter.localeCompare(char))
+        .join('');
+    return newWord[0].toUpperCase() + newWord.slice(1);
 }
 
-function sort(sentence) {
-    sentence = sentence.trim().split(' ').filter(word => word !== ''); // удаляем лишние пробелы
-    return sentence.map(sortLetters).sort((word, slovo) => word.localeCompare(slovo)).join(' ');
+/* Сортировка отдельных слов */
+const sort = (sentence) => {
+    if (typeof (sentence) !== "string") {
+        return "Необходимо ввести строку"
+    }
+    return sentence
+        .trim()
+        .split(' ')
+        .filter((word) => word !== '') // удаляем лишние пробелы
+        .map(sortLetters)
+        .sort((word, ans) => word.localeCompare(ans))
+        .join(' ');
 }
