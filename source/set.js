638c69f0cc94fc4ac634c9b7db9c50e710975bef
MaratKamalovPD <dead_island_hd@bk.ru>
2024-02-14 13:40:36 +0300

    some changes 2

--- source/set.js
+++ source/set.js
@@ -7,31 +7,37 @@
  * @param {string | Array<string>} path - Путь к свойству в виде строки или массива строк
  * @param {*} value - Значение, которое нужно установить
  * @returns {Object} - Исходный объект с установленным значением
+ * @throws {TypeError} - Если тип `path` некорректный (не является строкой или массивом строк)
  */
 const set = (object, path, value) => {
+  /**
+   * Разбирает путь к свойству и возвращает массив строк
+   *
+   * @param {string | Array<string>} path - Путь к свойству в виде строки или массива строк
+   * @returns {Array<string>} - Массив строк, представляющих путь к свойству
+   * @throws {TypeError} - Если тип `path` некорректный (не является строкой или массивом строк)
+   */
+  const parsePath = (path) => {
+    if (typeof path === 'number' || !path || typeof path === 'function' || (path instanceof Object && !Array.isArray(path))) {
+      throw new TypeError('Invalid path type');
+    };
+    if (Array.isArray(path)) {
+      return path;
+    } else {
+      if (path.startsWith('.')) {
+      path = path.substring(1);
+    }
+    return path.split('.');
+    };
+  };
+
   let currentObject = object;
   if (!object) {
     currentObject = {};
     object = currentObject;
-  }
+  };
 
-  if (typeof path === 'number' || path === null) {
-    return;
-  }
-
-  let properties
-   if (Array.isArray(path)) {
-    
-     properties = path;
-   }
-   else {
-      properties = path.split('.');
-      properties.shift();
-      if (path.substring(0, 1) != '.'){
-        return
-      }
-   };
-   
+  const properties = parsePath(path);
   const lastProperty = properties.pop();
 
   /**
@@ -41,16 +47,18 @@ const set = (object, path, value) => {
  * @param {string} property - Свойство, для которого нужно создать или получить объект.
  * @returns {Object} - Созданный или полученный объект для указанного свойства.
  */
-  const setObject = (object, property) => {
+  const finalObject = properties.reduce((object, property) => {
     if (!object.hasOwnProperty(property)) {
       object[property] = {};
     }
     return object[property];
-  };
-
-  const finalObject = properties.reduce(setObject, object);
+  }, object);
   finalObject[lastProperty] = value;
 
   return object;
 };
+
+
+
+
 
--- tests/set.js
+++ tests/set.js
@@ -117,9 +117,117 @@ QUnit.module('Тестируем функцию set', function () {
 	});
 
 	QUnit.test('set обрабатывает исключения, когда в path приходит что-то не то', function (assert) {
-		assert.deepEqual(set({foo: 'bar'}, null, 'baz'), undefined );
-		assert.deepEqual(set({foo: 'bar'}, 'foo', 'baz'), undefined);
-		assert.deepEqual(set({foo: 'bar'}, 2, 'baz'), undefined);
+		assert.throws(function() {set({ foo: 'bar' }, null, 'baz')}, TypeError, 'Invalid path type');
+		assert.throws(function() {set({ foo: 'bar' }, 2, 'baz')}, TypeError, 'Invalid path type');
 	});
+
+	QUnit.test('set работает правильно если переданный путь равен {}', function (assert) {
+
+		assert.throws(function() {set({ foo: 'bar' }, {}, 'baz')}, TypeError, 'Invalid path type');
+		assert.throws(function() {set({ foo: 'bar' }, {foo: 'bar'}, 'baz')}, TypeError, 'Invalid path type');
+	});
+
+	QUnit.test('set работает правильно если переданный путь представляет из себя функцию', function (assert) {
+
+		assert.throws(function() {set({ foo: 'bar' }, function(){return}, 'baz')}, TypeError, 'Invalid path type');
+	});
+
+    // тесты для случая, когда путь приходит без точки в начале
+	QUnit.test('set работает правильно c объектами с существующими свойствами при условии, что путь приходит без точки в начале', function (assert) {
+		const object = {
+			deep: {
+				hested: {
+					field: 'baz'
+				}
+			}
+		};
+
+		const object2 = {
+			deep: {
+				hested: {
+					field: 42
+				}
+			}
+		};
+
+		const object3 = {
+			deep: {
+				hested: {
+					foo: 'bar'
+				}
+			}
+		};
+
+		const object4 = {
+			deep: null
+		};
+
+		assert.deepEqual(set({foo: 'bar'}, 'foo', 'baz'), {foo: 'baz'});
+		assert.deepEqual(set(object, 'deep.hested.field', 42), object2);
+
+		assert.deepEqual(set(object, 'deep.hested', {foo: 'bar'}), object3);
+		assert.deepEqual(set(object, 'deep', null), object4);
+	});
+
+	QUnit.test('set изменяет переданный объект при условии, что путь приходит без точки в начале', function (assert) {
+		const object = {
+			foo: 'bar'
+		};
+
+		const object1 = {
+			foo: 'baz'
+		};
+
+		const object2 = set(object, 'foo', 'baz');
+		assert.deepEqual(object, object1);
+		assert.deepEqual(object2, object1);
+	});
+
+	QUnit.test('set работает правильно c массивами при условии, что путь приходит без точки в начале', function (assert) {
+		const object1 = {
+			foo: [ 1, 2, 3 ],
+			bar: [
+				{foobar: '42'}
+			]
+		};
+
+		const object2 = {
+			foo: [ 1, 2, 3 ],
+			bar: [
+				{foobar: '42'}
+			]
+		};
+
+		const new1 = {
+			foo: [ 42, 2, 3 ],
+			bar: [
+				{foobar: '42'}
+			]
+		};
+
+		const new2 = {
+			foo: [ 1, 2, 3 ],
+			bar: [
+				{foobar: 'baz'}
+			]
+		};
+
+		assert.deepEqual(set(object1, 'foo.0', 42), new1);
+		assert.deepEqual(set(object2, 'bar.0.foobar', 'baz'), new2);
+	});
+
+	QUnit.test('set работает правильно c объектами без свойств при условии, что путь приходит без точки в начале', function (assert) {
+		const object = {
+			deep: {
+				nested: {
+					field: null
+				}
+			}
+		};
+
+		assert.deepEqual(set({}, 'deep.nested.field', null), object);
+	});
+
+	
 
 });
