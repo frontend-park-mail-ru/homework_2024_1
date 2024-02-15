2aa396899385a735f13f0ce63912f27137560909
fztott <anton.zhidkov.01@mail.ru>
2024-02-15 19:11:55 +0300

    правки

--- source/minmax.js
+++ source/minmax.js
@@ -4,16 +4,12 @@
  * @returns {array} - Возвращает массив с минимальным и максимальным значением
  */
 const minmax = (numericString) => {
-	// if (numericString instanceof String) {
-    //     numericString = numericString.toString();
-    // }
-	
-	if (typeof numericString !== typeof ' ' && !(numericString instanceof String)) {
+	if (typeof numericString !== 'string' && !(numericString instanceof String)) {
 
 		return [undefined, undefined];
 	}
 
-	const subStringArray = numericString.split(/[,;:\s]+/).filter(num => num !== '');
+	const subStringArray = numericString.split(/[,;:\s]+/).filter(num => num!='');
 
 	//преобразование подстрок в значение
 	const numArray = subStringArray.map(Number).filter(num => !Number.isNaN(num));
