80a888739ed531cb683e575033d6d3091b7dd645
Artem Zhuk <artem.zx656@gmail.com>
2024-02-15 15:39:05 +0300

    refactored tests, fixed jsdoc

--- source/get.js
+++ source/get.js
@@ -1,17 +1,17 @@
 'use strict';
 
 /**
- * Получает путь к вложенному свойству объекта и возвращает значение этого свойства (или undefined, если свойства не существует)
- * @param {Object} object - Объект.
- * @param {string} property - свойство объекта.
- * @return {Object|undefined} - Возвращает значение или undefined.
+ * Получает путь к вложенному свойству объекта и возвращает значение этого свойства
+ * @param {Object} object - объект
+ * @param {string} property - свойство объекта
+ * @throws {ReferenceError} - указанное свойство не существует
+ * @return {any} - Возвращает свойство
  */
 const get = (object, property) => {
     let propertyArray = property.split('.').slice(1).filter(item => item.length > 0);
     let current = object;
     propertyArray.forEach((item) => {
         if (!current.hasOwnProperty(item)) {
-            current = undefined;
             throw ReferenceError(`${item} property is undefined`);
         }
         current = current[item];
--- tests/get.js
+++ tests/get.js
@@ -34,10 +34,7 @@ QUnit.module('Тестируем функцию get', function () {
 		assert.strictEqual(get(object, '.baz.length'), object.baz.length);
 		assert.strictEqual(get(object, '.deep.0.foobar'), object.deep[ 0 ].foobar);
 		assert.strictEqual(get(object, '.baz.3'), object.baz[ 3 ]);
-		assert.throws(
-			function(){ get(object, '.baz.4')},
-			function(err){ return err.name === 'ReferenceError' && err.message === '4 property is undefined'},
-		);
+		assert.throws(() => get(object, '.baz.4'), ReferenceError('4 property is undefined'),);
 	});
 
 	QUnit.test('get работает правильно c объектами без свойств', function (assert) {
@@ -46,45 +43,18 @@ QUnit.module('Тестируем функцию get', function () {
 				bar: 42
 			}
 		};
-		assert.throws(
-			function(){ get(object, '.foobar')},
-			function(err){ return err.name === 'ReferenceError' && err.message === 'foobar property is undefined'},
-		);
-		assert.throws(
-			function(){ get(object, '.foo.baz')},
-			function(err){ return err.name === 'ReferenceError' && err.message === 'baz property is undefined'},
-		);
-		assert.throws(
-			function(){ get(object, '.baz.0')},
-			function(err){ return err.name === 'ReferenceError' && err.message === 'baz property is undefined'},
-		);
-		assert.throws(
-			function(){ get(object, '.baz.length')},
-			function(err){ return err.name === 'ReferenceError' && err.message === 'baz property is undefined'},
-		);
-		assert.throws(
-			function(){ get(object, '.baz.length')},
-			function(err){ return err.name === 'ReferenceError' && err.message === 'baz property is undefined'},
-		);
-		assert.throws(
-			function(){ get(object, '.0.1.2')},
-			function(err){ return err.name === 'ReferenceError' && err.message === '0 property is undefined'},
-		);
+		assert.throws(() => get(object, '.foobar'), ReferenceError('foobar property is undefined'),);
+		assert.throws(() => get(object, '.foo.baz'), ReferenceError('baz property is undefined'),);
+		assert.throws(() => get(object, '.baz.0'), ReferenceError('baz property is undefined'),);
+		assert.throws(() => get(object, '.baz.length'), ReferenceError('baz property is undefined'),);
+		assert.throws(() => get(object, '.baz.length'), ReferenceError('baz property is undefined'),);
+		assert.throws(() => get(object, '.0.1.2'), ReferenceError('0 property is undefined'),);
 	});
 
 	QUnit.test('get работает правильно c пустыми объектами', function (assert) {
 		const object = {};
-		assert.throws(
-			function(){ get(object, '.foobar')},
-			function(err){ return err.name === 'ReferenceError' && err.message === 'foobar property is undefined'},
-		);
-		assert.throws(
-			function(){ get(object, '.length')},
-			function(err){ return err.name === 'ReferenceError' && err.message === 'length property is undefined'},
-		);
-		assert.throws(
-			function(){ get(object, '.foobar.length') },
-			function(err){ return err.name === 'ReferenceError' && err.message === 'foobar property is undefined'},
-		);
+		assert.throws(() => get(object, '.foobar'), ReferenceError('foobar property is undefined'),);
+		assert.throws(() => get(object, '.length'), ReferenceError('length property is undefined'),);
+		assert.throws(() => get(object, '.foobar.length'), ReferenceError('foobar property is undefined'),);
 	});
 });
