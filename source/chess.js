b74bababc218a818bc5cc4bdc6a2e8725032aaf8
DeDeimos <rodion.tolkachev.03@inbox.ru>
2024-02-16 19:02:09 +0300

    new changes

--- source/chess.js
+++ source/chess.js
@@ -1,21 +1,21 @@
 "use strict";
 
-function chess(n) {
-  if (n < 2 || isNaN(n) || n === Infinity) {
+const chess = (n) => {
+  if (n < 2 || isNaN(n) || n === Infinity ) {
     return null;
   }
 
-  let oddRow = "";
-  let evenRow = "";
-  let matrix = "";
+  let oddRow = '';
+  let evenRow = '';
+  let matrix = '';
 
   for (let i = 0; i < n; i++) {
-    oddRow += i % 2 === 0 ? "*" : " ";
-    evenRow += i % 2 === 0 ? " " : "*";
+    oddRow += i % 2 === 0 ? '*' : ' ';
+    evenRow += i % 2 === 0 ? ' ' : '*';
   }
 
-  oddRow += "\n";
-  evenRow += "\n";
+  oddRow += '\n';
+  evenRow += '\n';
   
   for (let i = 0; i < n; i++) {
     matrix += i % 2 === 0 ? oddRow : evenRow;
