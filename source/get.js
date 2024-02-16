8760b0a710b0449dddfa1b3dc9953d711dc0b54b
Artem Zhuk <artem.zx656@gmail.com>
2024-02-16 12:35:13 +0300

    changed jsdoc, changed array declaration

--- source/get.js
+++ source/get.js
@@ -5,10 +5,14 @@
  * @param {Object} object - объект
  * @param {string} property - свойство объекта
  * @throws {ReferenceError} - указанное свойство не существует
+ * @throws {TypeError} - параметер property не является строкой
  * @return {any} - Возвращает свойство
  */
 const get = (object, property) => {
-    let propertyArray = property.split('.').slice(1).filter(item => item.length > 0);
+    if (typeof property !== 'string'){
+        throw TypeError('Property is not a string')
+    }
+    const propertyArray = property.split('.').slice(1).filter(item => item.length > 0);
     let current = object;
     propertyArray.forEach((item) => {
         if (!current.hasOwnProperty(item)) {
