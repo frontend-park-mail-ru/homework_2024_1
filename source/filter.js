a7f538903d3d2fd2213c6e7853813bfc23593be5
Иван Карпов <ivankarpov@MacBook-Pro-Ivan.local>
2024-02-18 23:48:54 +0300

    Внесение правок

--- source/filter.js
+++ source/filter.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * Filters the input text by allowing specified HTML tags and unescaping them.
@@ -9,19 +9,19 @@
  * @throws {Error} If input is not a string or allowedTags is not an array.
  */
 const filter = (input, allowedTags) => {
-  if (typeof input !== "string") {
-    throw new Error("Invalid input. 'input' must be a string.");
+  if (typeof input !== 'string' && !(input instanceof String)) {
+    throw new TypeError('Invalid input. \'input\' must be a string.');
   }
 
   if (!Array.isArray(allowedTags)) {
-    throw new Error("Invalid input. 'allowedTags' must be an array.");
+    throw new TypeError('Invalid input. \'allowedTags\' must be an array.');
   }
 
   let escapedText = escapeHTML(input);
 
   allowedTags.forEach((tag) => {
-    const openingTagPattern = new RegExp(`&lt;${tag}&gt;`, "g");
-    const closingTagPattern = new RegExp(`&lt;/${tag}&gt;`, "g");
+    const openingTagPattern = new RegExp(`&lt;${tag}&gt;`, 'g');
+    const closingTagPattern = new RegExp(`&lt;/${tag}&gt;`, 'g');
 
     escapedText = escapedText
       .replace(openingTagPattern, `<${tag}>`)
@@ -39,21 +39,9 @@ const filter = (input, allowedTags) => {
  */
 function escapeHTML(text) {
   return text
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#39;");
-}
-
-try {
-  console.log(filter(1, ["b"]));
-} catch (error) {
-  console.error(error.message);
-}
-
-try {
-  console.log(filter("<script>alert('XSS');</script>", 1));
-} catch (error) {
-  console.error(error.message);
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 }
--- tests/filter.js
+++ tests/filter.js
@@ -1,72 +1,62 @@
-"use strict";
+'use strict';
 
-QUnit.module("Проверка работы функции filter", function () {
-  QUnit.test("filter экранирует символы в обычном тексте", function (assert) {
-    const input =
-      '- "42!", сказала Машина. Это и был главный ответ на Вопрос жизни, вселенной & всего такого...';
+QUnit.module('Проверка работы функции filter', function () {
+	QUnit.test('filter экранирует символы в обычном тексте', function (assert) {
+		const input = '- "42!", сказала Машина. Это и был главный ответ на Вопрос жизни, вселенной & всего такого...';
 
-    const output = filter(input, ["strong", "em"]);
+		const output = filter(input, [ 'strong', 'em' ]);
 
-    const expected =
-      "- &quot;42!&quot;, сказала Машина. Это и был главный ответ на Вопрос жизни, вселенной &amp; всего такого...";
+		const expected = '- &quot;42!&quot;, сказала Машина. Это и был главный ответ на Вопрос жизни, вселенной &amp; всего такого...';
 
-    assert.strictEqual(output, expected);
+		assert.strictEqual(output, expected);
+	});
+
+	QUnit.test('filter не экранирует валидные html-тэги', function (assert) {
+		const input = '<strong>Hello, <em>World!</em></strong> 1 + 2 < 4!';
+
+		const output = filter(input, [ 'strong', 'em' ]);
+
+		const expected = '<strong>Hello, <em>World!</em></strong> 1 + 2 &lt; 4!';
+
+		assert.strictEqual(output, expected);
+	});
+
+	QUnit.test('filter экранирует XSS', function (assert) {
+		assert.strictEqual(filter(`<script>alert('1');</script>`, [ 'strong', 'em' ]), '&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;');
+		assert.strictEqual(filter(`<img src="bad" onerror="alert('1');">`, [ 'strong', 'em' ]), '&lt;img src=&quot;bad&quot; onerror=&quot;alert(&#39;1&#39;);&quot;&gt;');
+	});
+
+  QUnit.test('filter обрабатывает пустую строку', function (assert) {
+    assert.strictEqual(filter('', ['strong', 'em']), '');
   });
 
-  QUnit.test("filter не экранирует валидные html-тэги", function (assert) {
-    const input = "<strong>Hello, <em>World!</em></strong> 1 + 2 < 4!";
+  QUnit.test('filter обрабатывает текст с разрешенными тегами', function (assert) {
+      const input = '<strong><em>Hello!</em></strong>';
 
-    const output = filter(input, ["strong", "em"]);
+      const output = filter(input, ['strong', 'em']);
 
-    const expected = "<strong>Hello, <em>World!</em></strong> 1 + 2 &lt; 4!";
+      const expected = '<strong><em>Hello!</em></strong>';
 
-    assert.strictEqual(output, expected);
-  });
-
-  QUnit.test("filter экранирует XSS", function (assert) {
-    assert.strictEqual(
-      filter(`<script>alert('1');</script>`, ["strong", "em"]),
-      "&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;"
-    );
-    assert.strictEqual(
-      filter(`<img src="bad" onerror="alert('1');">`, ["strong", "em"]),
-      "&lt;img src=&quot;bad&quot; onerror=&quot;alert(&#39;1&#39;);&quot;&gt;"
-    );
-  });
-
-  QUnit.test("filter обрабатывает пустую строку", function (assert) {
-    assert.strictEqual(filter("", ["strong", "em"]), "");
-  });
-
-  QUnit.test(
-    "filter обрабатывает текст с разрешенными тегами",
-    function (assert) {
-      const input = "<strong><em>Hello!</em></strong>";
-      const output = filter(input, ["strong", "em"]);
-      const expected = "<strong><em>Hello!</em></strong>";
       assert.strictEqual(output, expected);
-    }
-  );
+  });
 
-  QUnit.test(
-    "filter обрабатывает ошибку, где вместо текста передали число",
-    function (assert) {
+  QUnit.test('filter обрабатывает ошибку, где вместо текста передали число', function (assert) {
       const input = 1;
-      const expected = "Invalid input. 'input' must be a string.";
-      assert.throws(() => {
-        filter(input, ["strong", "em"]);
-      }, new Error(expected));
-    }
-  );
 
-  QUnit.test(
-    "filter обрабатывает ошибку, где вместо массива тэгов передали число",
-    function (assert) {
-      const input = "<strong><em>Hello!</em></strong>";
-      const expected = "Invalid input. 'allowedTags' must be an array.";
+      const expected = 'Invalid input. \'input\' must be a string.';
+
+      assert.throws(() => {
+        filter(input, ['strong', 'em']);
+      }, new TypeError(expected));
+  });
+
+  QUnit.test('filter обрабатывает ошибку, где вместо массива тэгов передали число', function (assert) {
+      const input = '<strong><em>Hello!</em></strong>';
+
+      const expected = 'Invalid input. \'allowedTags\' must be an array.';
+
       assert.throws(() => {
         filter(input, 1);
-      }, new Error(expected));
-    }
-  );
+      }, new TypeError(expected));
+  });
 });
