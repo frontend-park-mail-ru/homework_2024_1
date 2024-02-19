23105c9379b02d97816cc39fb19ac06610f4b9f6
Oleg Zhdanov <helge2003@yandex.ru>
2024-02-19 20:27:27 +0300

    docs

--- source/rle.js
+++ source/rle.js
@@ -1,10 +1,19 @@
+"use strict";
+
+/**
+ * Returns true if arguement is a String
+ */
 const isString = (obj) => {
     return typeof obj === 'string' || obj instanceof String; 
 }
 
+/**
+ * Applies RLE data compression algorithm to argument and returns resulting string.
+ * Throw TypeError if argument is not a string or missing.
+ */
 const rle = (input) => {
     if (!isString(input)) {
-	throw new Error('Parameter is not a string or missing!');
+	throw new TypeError('Argument is not a string or missing!');
     }
     let result = '';
     let count = 1;
@@ -22,4 +31,3 @@ const rle = (input) => {
 
     return result;
 }
-
--- tests/rle.js
+++ tests/rle.js
@@ -1,11 +1,10 @@
 'use strict';
 
 QUnit.module('Тестируем функцию rle', function () {
-    QUnit.test('Базовая функциональность работает правильно', function (assert) {
+    QUnit.test('rle работает правильно', function (assert) {
 	assert.strictEqual(rle('AAAB'), 'A3B');
 	assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
 	assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
-	assert.strictEqual(rle('AAAAAAAAAA'), 'A10');
     });
     QUnit.test('Работает правильно с единичными символами', function (assert) {
 	assert.strictEqual(rle('A'), 'A');
@@ -15,11 +14,11 @@ QUnit.module('Тестируем функцию rle', function () {
 	assert.strictEqual(rle(''), '');
     });
     QUnit.test('Работает правильно без параметра', function (assert) {
-	assert.throws(function () {rle()}, /Parameter is not a string or missing!/);
+	assert.throws(function () {rle()}, /Argument is not a string or missing!/);
     });
     QUnit.test('Работает правильно с неверным параметром', function (assert) {
-	assert.throws(function () {rle(111)}, /Parameter is not a string or missing!/);
-	assert.throws(function () {rle([1, 2, 3])}, /Parameter is not a string or missing!/);
-	assert.throws(function () {rle(NaN)}, /Parameter is not a string or missing!/);
+	assert.throws(function () {rle(111)}, /Argument is not a string or missing!/);
+	assert.throws(function () {rle([1, 2, 3])}, /Argument is not a string or missing!/);
+	assert.throws(function () {rle(NaN)}, /Argument is not a string or missing!/);
     });
 });
