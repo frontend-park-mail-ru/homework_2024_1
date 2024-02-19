e956c4107244fef95d9bb38f3dad49b5f0224373
Olegsandrik <Nikitinnvlalex@gmail.com>
2024-02-19 13:21:31 +0300

    Правки от Дани

--- source/tree.js
+++ source/tree.js
@@ -1,17 +1,22 @@
 'use strict';
+/**
+ * Функция, рисующая елку.
+ * @param {number} number - Число, определяющее высоту елки.
+ * @returns {string} treepicture - Изображение елки.
+ */
 const tree = (number) => {
     if (number<3 || number!=Math. trunc(number) || !isFinite(number)){
         return null;
     }
 
-    let answer = '';
+    let treepicture = '';
     let star = 1;
     for (let i = 0; i < number-1; i++) {
-        answer+=' '.repeat(number-2-i) +  '*'.repeat(star) + ' '.repeat(number-2-i) + '\n';
+        treepicture+=' '.repeat(number-2-i) +  '*'.repeat(star) + ' '.repeat(number-2-i) + '\n';
         star+=2;
     }
-    answer+=' '.repeat(star/2-1) + '|' + ' '.repeat(star/2-1) + '\n';
+    treepicture+=' '.repeat(star/2-1) + '|' + ' '.repeat(star/2-1) + '\n';
 
-    return answer;
+    return treepicture;
 }
 
