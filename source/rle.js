975250e37bd8db51aba549c6d4bbab7108260c84
Oleg Zhdanov <helge2003@yandex.ru>
2024-02-21 20:35:17 +0300

    yaf

--- source/rle.js
+++ source/rle.js
@@ -1,33 +1,35 @@
-"use strict";
+'use strict';
 
 /**
  * Returns true if arguement is a String
+ * @param {object} obj - object to check.
  */
 const isString = (obj) => {
-    return typeof obj === 'string' || obj instanceof String; 
+	return typeof obj === 'string' || obj instanceof String; 
 }
 
 /**
  * Applies RLE data compression algorithm to argument and returns resulting string.
  * Throw TypeError if argument is not a string or missing.
+ * @param {string} input - string to be compressed.
  */
 const rle = (input) => {
-    if (!isString(input)) {
-	throw new TypeError('Argument is not a string or missing!');
-    }
-    let result = '';
-    let count = 1;
-    for (let i = 1; i <= input.length; i++) {
-	if (input[i] === input[i - 1]) {
-	    count++;
-	} else {
-	    result += input[i - 1];
-	    if (count > 1) {
-		result += count;
-	    }
-	    count = 1;
+	if (!isString(input)) {
+		throw new TypeError('Argument is not a string or missing!');
 	}
-    }
+	let result = '';
+	let count = 1;
+	for (let i = 1; i <= input.length; i++) {
+		if (input[i] === input[i - 1]) {
+			count++;
+		} else {
+			result += input[i - 1];
+			if (count > 1) {
+				result += count;
+			}
+			count = 1;
+		}
+	}
 
-    return result;
+	return result;
 }
--- tests/rle.js
+++ tests/rle.js
@@ -1,28 +1,28 @@
 'use strict';
 
 QUnit.module('Тестируем функцию rle', function () {
-    QUnit.test('rle работает правильно', function (assert) {
-	assert.strictEqual(rle('AAAB'), 'A3B');
-	assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
-	assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
-    });
-    QUnit.test('Работает правильно с единичными символами', function (assert) {
-	assert.strictEqual(rle('A'), 'A');
-	assert.strictEqual(rle('ABC'), 'ABC');
-    });
-    QUnit.test('Работает правильно с пустой строкой', function (assert) {
-	assert.strictEqual(rle(''), '');
-    });
-    QUnit.test('Работает правильно со строками, созданными оператором new', function (assert) {
-	assert.strictEqual(rle(new String('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD')), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
-	assert.strictEqual(rle(new String('BCCDDDAXXXX')), 'BC2D3AX4');
-    });
-    QUnit.test('Работает правильно без параметра', function (assert) {
-	assert.throws(function () {rle()}, /Argument is not a string or missing!/);
-    });
-    QUnit.test('Работает правильно с неверным параметром', function (assert) {
-	assert.throws(function () {rle(111)}, /Argument is not a string or missing!/);
-	assert.throws(function () {rle([1, 2, 3])}, /Argument is not a string or missing!/);
-	assert.throws(function () {rle(NaN)}, /Argument is not a string or missing!/);
-    });
+	QUnit.test('rle работает правильно', function (assert) {
+		assert.strictEqual(rle('AAAB'), 'A3B');
+		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
+		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
+	});
+	QUnit.test('Работает правильно с единичными символами', function (assert) {
+		assert.strictEqual(rle('A'), 'A');
+		assert.strictEqual(rle('ABC'), 'ABC');
+	});
+	QUnit.test('Работает правильно с пустой строкой', function (assert) {
+		assert.strictEqual(rle(''), '');
+	});
+	QUnit.test('Работает правильно со строками, созданными оператором new', function (assert) {
+		assert.strictEqual(rle(new String('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD')), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
+		assert.strictEqual(rle(new String('BCCDDDAXXXX')), 'BC2D3AX4');
+	});
+	QUnit.test('Работает правильно без параметра', function (assert) {
+		assert.throws(function () {rle()}, /Argument is not a string or missing!/);
+	});
+	QUnit.test('Работает правильно с неверным параметром', function (assert) {
+		assert.throws(function () {rle(111)}, /Argument is not a string or missing!/);
+		assert.throws(function () {rle([1, 2, 3])}, /Argument is not a string or missing!/);
+		assert.throws(function () {rle(NaN)}, /Argument is not a string or missing!/);
+	});
 });
