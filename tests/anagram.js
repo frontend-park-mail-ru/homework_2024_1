714eaea5032958008dbe1d880544c1e152ddbd55
kond9 <plenkin.2018@gmail.com>
2024-02-25 17:53:14 +0300

    edit tab's in code

--- tests/anagram.js
+++ tests/anagram.js
@@ -1,83 +1,83 @@
 'use strict';
 
 QUnit.module('Тестируем функцию anagram', function () {
-  QUnit.test('Функция работает правильно', function (assert) {
-    const input = [
-      'кот', 'пила', 'барокко',
-      'стоп', 'ток', 'кошка',
-      'липа', 'коробка', 'пост'
-    ];
+        QUnit.test('Функция работает правильно', function (assert) {
+                const input = [
+                        'кот', 'пила', 'барокко',
+                        'стоп', 'ток', 'кошка',
+                        'липа', 'коробка', 'пост'
+                ];
 
-    const output = [
-      ['барокко', 'коробка'],
-      ['кот', 'ток'],
-      ['липа', 'пила'],
-      ['пост', 'стоп']
-    ];
+                const output = [
+                        ['барокко', 'коробка'],
+                        ['кот', 'ток'],
+                        ['липа', 'пила'],
+                        ['пост', 'стоп']
+                ];
 
-    assert.deepEqual(anagram(input), output);
-  });
+                assert.deepEqual(anagram(input), output);
+        });
 
-  QUnit.test('Возвращает правильно отсортированные слова', function (assert) {
-    const input = [
-      'апорт', 'торг', 'тропа',
-      'стоп', 'грот'
-    ];
+        QUnit.test('Возвращает правильно отсортированные слова', function (assert) {
+                const input = [
+                        'апорт', 'торг', 'тропа',
+                        'стоп', 'грот'
+                ];
 
-    const output = [
-      ['апорт', 'тропа'],
-      ['грот', 'торг']
-    ];
+                const output = [
+                        ['апорт', 'тропа'],
+                        ['грот', 'торг']
+                ];
 
-    assert.deepEqual(anagram(input), output);
-  });
+                assert.deepEqual(anagram(input), output);
+        });
 
-  QUnit.test('Функция работает правильно, когда нет анаграмм', function (assert) {
-    const input = [
-      'кот', 'пила', 'барокко',
-      'стоп'
-    ];
+        QUnit.test('Функция работает правильно, когда нет анаграмм', function (assert) {
+                const input = [
+                        'кот', 'пила', 'барокко',
+                        'стоп'
+                ];
 
-    const output = [];
+                const output = [];
 
-    assert.deepEqual(anagram(input), output);
-  });
+                assert.deepEqual(anagram(input), output);
+        });
 
-  QUnit.test('Функция корректно обрабатывает массив разных типов данных', function (assert) {
-    const input = [
-      undefined, 'кот', 1, 'пила', null, 'барокко',
-      'стоп', 'ток', 'кошка', 'липа', {}, 'коробка', new String('пост')
-    ];
+        QUnit.test('Функция корректно обрабатывает массив разных типов данных', function (assert) {
+                const input = [
+                        undefined, 'кот', 1, 'пила', null, 'барокко',
+                        'стоп', 'ток', 'кошка', 'липа', {}, 'коробка', new String('пост')
+                ];
 
-    const output = [
-      ['барокко', 'коробка'],
-      ['кот', 'ток'],
-      ['липа', 'пила'],
-      ['пост', 'стоп']
-    ];
+                const output = [
+                        ['барокко', 'коробка'],
+                        ['кот', 'ток'],
+                        ['липа', 'пила'],
+                        ['пост', 'стоп']
+                ];
 
-    assert.deepEqual(anagram(input), output);
-  });
+                assert.deepEqual(anagram(input), output);
+        });
 
 
-  QUnit.test('Функция выбрасывает ошибку при передаче null', function (assert) {
-    assert.throws(() => anagram(null), new TypeError('Input should be an array!!!'));
-  });
+        QUnit.test('Функция выбрасывает ошибку при передаче null', function (assert) {
+                assert.throws(() => anagram(null), new TypeError('Input should be an array!!!'));
+        });
 
-  QUnit.test('Функция выбрасывает ошибку при передаче undefined', function (assert) {
-    assert.throws(() => anagram(undefined), new TypeError('Input should be an array!!!'));
-  });
+        QUnit.test('Функция выбрасывает ошибку при передаче undefined', function (assert) {
+                assert.throws(() => anagram(undefined), new TypeError('Input should be an array!!!'));
+        });
 
-  QUnit.test('Функция выбрасывает ошибку при передаче строки', function (assert) {
-    assert.throws(() => anagram('not an array'), new TypeError('Input should be an array!!!'));
-  });
+        QUnit.test('Функция выбрасывает ошибку при передаче строки', function (assert) {
+                assert.throws(() => anagram('not an array'), new TypeError('Input should be an array!!!'));
+        });
 
-  QUnit.test('Функция выбрасывает ошибку при передаче числа', function (assert) {
-    assert.throws(() => anagram(123456), new TypeError('Input should be an array!!!'));
-  });
+        QUnit.test('Функция выбрасывает ошибку при передаче числа', function (assert) {
+                assert.throws(() => anagram(123456), new TypeError('Input should be an array!!!'));
+        });
 
-  QUnit.test('Функция выбрасывает ошибку при передаче объекта', function (assert) {
-    assert.throws(() => anagram({}), new TypeError('Input should be an array!!!'));
-  });
+        QUnit.test('Функция выбрасывает ошибку при передаче объекта', function (assert) {
+                assert.throws(() => anagram({}), new TypeError('Input should be an array!!!'));
+        });
 
 });
