f32273f24d6aba720f536a69c1d27eb8ed1d00d6
kisssick0 <100334382+kisssick0@users.noreply.github.com>
2024-02-27 02:32:48 +0300

    fix func

--- source/plainify.js
+++ source/plainify.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const plainify = function plainify(obj, parentKey = '', result = {}) {
-    for (let key in obj) {
+const plainify = (obj, parentKey = '', result = {}) => {
+    for (const key in obj) {
         if (typeof obj[key] === 'object' && obj[key] !== null) {
             plainify(obj[key], parentKey + key + '.', result);
         } else {
